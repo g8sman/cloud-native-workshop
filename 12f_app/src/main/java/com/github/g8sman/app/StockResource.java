5387a6ce52641c3fe6b2a3e82843d8e7a1dd8e89
Jon Gates <Jon.Gates@sas.com>
2017-03-29 16:35:08 -0400

    Added logging

--- 12f_app/src/main/java/com/github/g8sman/app/StockResource.java
+++ 12f_app/src/main/java/com/github/g8sman/app/StockResource.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
@@ -23,17 +25,23 @@ public class StockResource {
 
 	@Autowired
 	private StockItemRepository stockItemRepository;
+	
+	private static final Logger logger = LoggerFactory.getLogger(StockResource.class);
 
 	@RequestMapping(method = RequestMethod.GET, value = "/{id}")
 	public StockItem stockItem(@PathVariable("id") Long id, HttpServletResponse response) {
 
+	    logger.info("Starting search of stock item(id={}) search", id);
+
 	    StockItem stockItem = stockItemRepository.findOne(id);
 
 	    if (stockItem == null) {
+	        logger.info("Stock item(id={}) has not been found", id);
 	        response.setStatus(HttpStatus.NOT_FOUND.value());
 	        return null;
 	    }
 
+	    logger.info("Finishing search of stock item(id={}) search", id);
 	    return stockItem;
 	}
 
